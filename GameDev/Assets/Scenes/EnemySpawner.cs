3d21c390b600ad8624d9d5ffc31d9a0ca9bdb786
Amanda <10932525@uvu.edu>
2022-09-02 20:23:00 -0600

    Scriptable Objects

---
+++ GameDev/Assets/Scenes/EnemySpawner.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class EnemySpawner : ScriptableObject
+{
+    public GameObject enemy;
+    public int spawnAmount = 5;
+    public float spawnDelay = 0.5f;
+    //Code by DROEI https://www.youtube.com/watch?v=uvIekOP07tU
+    
+    
+}
