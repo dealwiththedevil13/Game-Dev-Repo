5d8c6fa8b8a81c199ae5945dfd3ccdbbec7a5017
Amanda <10932525@uvu.edu>
2022-09-09 23:08:53 -0600

    Trying to create a floating pickup.
    Needs another edit

--- GameDev/Assets/Scenes/FloatData.cs
+++ GameDev/Assets/Scenes/FloatData.cs
@@ -10,4 +10,6 @@ public class FloatData : ScriptableObject
     {
         value += num;
     }
+
+    
 }
--- GameDev/Assets/Scenes/MonoBehavior.cs
+++ GameDev/Assets/Scenes/MonoBehavior.cs
@@ -4,9 +4,36 @@ using UnityEngine;
 
 public class MonoBehavior : MonoBehaviour
 {
+    public UnityEvent startEvent, awakeEvent, disableEvent;
+
+   private void Awake()
+   {
+        awakeEvent.Invoke();
+   }
+
+    private void Start()
+   {
+        startEvent.Invoke();
+   }
+
+    private void OnDisable()
+   {
+        disableEvent.Invoke();
+   }
+   
+ //Up and down motion. Still in the works.Get help from tutors
+   public void Update()
+    {
+        transform.position = new Vector3(0, Mathf.Sin(Time.time), 0);
+    }
+   
    //Destroy pickup on contact
    public void OnTriggerEnter(Collider other)
   {
       Destroy(gameObject);
+  }
+  public void OnDestroy()
+  {
+
   }
 }
